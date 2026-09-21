1890743bb48c3c8a4c1c9e0e2f223ed1d23dcb38
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2026-09-21 12:07:28 -0400

    added builder i forgot

--- src/main/java/com/tryingstuff/stuff/guild/blizzapi/BlizzardConfig.java
+++ src/main/java/com/tryingstuff/stuff/guild/blizzapi/BlizzardConfig.java
@@ -2,9 +2,16 @@ package com.tryingstuff.stuff.guild.blizzapi;
 
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 @EnableConfigurationProperties(BlizzardProperties.class)
 public class BlizzardConfig {
+
+    @Bean
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 }
