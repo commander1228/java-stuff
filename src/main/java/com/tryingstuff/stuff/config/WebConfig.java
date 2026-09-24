fb598fbbe616a5f0d9b5f4af608f13b9564227ad
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2026-09-24 00:14:40 -0400

    added vball signup to allowed origins
    should fix this to only let specific origins for specific domains or something

--- src/main/java/com/tryingstuff/stuff/config/WebConfig.java
+++ src/main/java/com/tryingstuff/stuff/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://trailer-park-guild-website.vercel.app","https://java-stuff-website.vercel.app")
+                .allowedOrigins("https://trailer-park-guild-website.vercel.app",
+                        "https://java-stuff-website.vercel.app",
+                        "https://volleyball-signup-5ktc.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
